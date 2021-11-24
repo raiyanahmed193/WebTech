3113905e62d46ab6ff912eee480e183525ceffde
raiyanahmed193 <raiyanahmedmahir@gmail.com>
2021-11-24 20:51:43 +0600

    3rd

--- FINAL_LAB_PERFORMANCE_2/Calculator.php
+++ FINAL_LAB_PERFORMANCE_2/Calculator.php
@@ -1,3 +1,18 @@
 <?php
-    $_POST['formText'];
+ $i1 = $_POST['name'];
+ $i2 = $_POST['id'];
+ $op = $_POST['op'];
+ switch($op)
+ {
+   case "+":
+   $output = $i1 + $i2;
+   echo $output;
+   break;
+   case "-":
+   $output = $i1 - $i2;
+   echo $output;
+   break;
+   case '*':
+ }
 ?>
+© 2021 GitHub, Inc.
