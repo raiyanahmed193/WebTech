b464474dba834ace977749b03c9ac7161b30e641
raiyanahmed193 <raiyanahmedmahir@gmail.com>
2021-11-24 20:58:14 +0600

    Final

--- FINAL_LAB_PERFORMANCE_2/Calculator.php
+++ FINAL_LAB_PERFORMANCE_2/Calculator.php
@@ -13,6 +13,24 @@
    echo $output;
    break;
    case '*':
+   $output = $i1 * $i2;
+   echo $output;
+   break;
+   case '/':
+   $output = $i1 / $i2;
+   echo $output;
+   break;
+   case '%':
+   $output = $i1 % $i2;
+   echo $output;
+   break;
+   case '++':
+   echo $i1 ++;
+   break;
+   case '--':
+   echo --$i1;
+   break;
+   default:
+   echo $output = "";
  }
 ?>
-© 2021 GitHub, Inc.
